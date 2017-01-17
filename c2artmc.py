73430eff78a003831b1ccd0f1fc53a432ce2f095
Matej Marusak <marusak.matej@gmail.com>
2017-01-17 19:01:14 +0100

    Add main file for running C2ARTMC
    Signed-off-by: Matej Marusak <marusak.matej@gmail.com>

---
+++ c2artmc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from src.parser import Parser
+from src.tokens import TokenEnum, TokenType
+
+
+def parse_arguments():
+    """Parse arguments.
+
+    Return object of all arguments.
+    """
+    parser = argparse.ArgumentParser(description="Converter from C to ARTMC.")
+    parser.add_argument("INPUT_FILE",
+                        help="C source file to be converted")
+    parser.add_argument("-o",
+                        help="Filepath to write the ARTMC file")
+
+    return parser.parse_args()
+
+
+def main():
+    """The main function."""
+    args = parse_arguments()
+
+
+if __name__ == "__main__":
+    main()
