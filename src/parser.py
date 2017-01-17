0ab5821b50b529419040906965020e19d7aaceda
Matej Marusak <marusak.matej@gmail.com>
2017-01-17 20:25:16 +0100

    Enhance functionality of parser
    Now can parser decide which of possible structures is present.
    We can only have (at least for now) structure, typedef of structure or
    function.
    
    Signed-off-by: Matej Marusak <marusak.matej@gmail.com>

--- src/parser.py
+++ src/parser.py
@@ -3,7 +3,7 @@
 Parse the whole file and return writable structure of ARTMC instructions.
 """
 
-from src.tokens import TokenEnum, TokenType
+from src.tokens import TokenEnum, TokenGroups
 from src.scanner import Scanner
 import src.error
 
@@ -18,8 +18,27 @@ class Parser:
 
     def run(self):
         """Parse the file and convert to ARTMC instructions."""
-        pass
+        t = self.s.get_token()
+        while (t != TokenEnum.XEOF):
+            if (t == TokenEnum.KWStruct):
+                self.parse_struct()
+            elif (t == TokenEnum.KWTypedef):
+                self.parse_typedef()
+            elif (t in TokenGroups.DataTypes):
+                pass
+            else:
+                src.error.FatalError("Unknown construction on line {0}."
+                                     .format(self.s.get_current_line()))
+            t = self.s.get_token()
 
     def get_artmc(self):
         """Return converted ARTMC instructions."""
         return self.instructions
+
+    def parse_struct(self):
+        """Parse one structure."""
+        pass
+
+    def parse_typedef(self):
+        """Parse one typedef."""
+        pass
